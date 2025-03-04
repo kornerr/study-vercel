0241db9cb41feace3a57bd908bef523246aa1567
Михаил Капелько <m.kapelko@iva-tech.ru>
2025-03-04 22:56:12 +0300

    d

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-#const express = require('express')
-#const app = express()
-#const port = 3000
-#
-#app.get("/", (req, res) => {
-#  res.send("Yo, Hello, World! I'm sooo express")
-#})
-#
-#app.listen(port, () => {
-#  console.log(`Example app listening on port ${port}`)
-#})
+// Taken as is from https://github.com/isomorphic-git/cors-proxy
 const express = require('express')
 const corsProxy = require('@isomorphic-git/cors-proxy/middleware.js')
 
@@ -16,3 +6,18 @@ const app = express()
 const options = {}
 
 app.use(corsProxy(options))
+
+
+/*
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Yo, Hello, World! I'm sooo express")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+*/
