7c70ccc5c18309273c4f8618641a377582e391bc
Михаил Капелько <m.kapelko@iva-tech.ru>
2025-03-04 23:19:33 +0300

    d

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require('express')
 const corsProxy = require('@isomorphic-git/cors-proxy/middleware.js')
 
 const app = express()
+const port = 3000
 const options = {}
 
 app.use(corsProxy(options))
 
+app.get("/", (req, res) => {
+  res.send("Yo, Hello, World! I'm sooo express")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 /*
 const express = require('express')
